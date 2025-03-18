f665e164461bc1a81ea68c3373cb1c8e719fbede
Pallavi <pan585@usask.ca>
2025-03-18 10:37:56 -0600

    generated output-log file

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 import torch
 
+torch.cuda.empty_cache()
+
 """
 Loading pretrained DeepSeek-V2 model from hugging face.
 Using AutoModelForCausalLM to generate output purely based on input
@@ -9,15 +11,18 @@ torch_dtype and device_map is set to auto for optimal resource usage,
 modify these values as per system and model needs
 """
 
-model_id = "deepseek-ai/DeepSeek-V2"
+model_id = "deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct"
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
 
+# print(model.config)
+
 # text generation pipeline
 
 generation_pipeline = pipeline(task="text-generation",
-                                model=model,
-                                tokenizer=tokenizer,
-                                trust_remote_code=True)
+                               model=model,
+                               tokenizer=tokenizer,
+                               batch_size=1,
+                               trust_remote_code=True)
 generation_pipeline("Hello, What are you?", max_new_tokens=25)
