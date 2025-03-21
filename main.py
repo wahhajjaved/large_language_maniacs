79a9b26173a5e4424d8c5b00cb485ca41b60fbae
Pallavi <pan585@usask.ca>
2025-03-21 15:10:11 -0600

    Load raw data from files in the ctssb dataset

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ from datasets import load_dataset
 import pytorch
 from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorWithPadding
 
+path = "datasets/ctssb/"
+raw_data = []
+for i in range (0, 64):
+    with open(path+i+"_before.py", r) as beforef:
+        raw_data[i] = {"before": read(beforef)}
 
-raw_data = load_dataset("datasets/ctssb")
-print(raw_data)
+    with open(path+i+"_after.py", r) as afterf:
+        raw_data[i] = {"after": read(afterf)}
+
+
