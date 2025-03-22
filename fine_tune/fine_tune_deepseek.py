d4a0359a728b72d0fb7da84847bbe775fb678672
Wahhaj Javed <muj975@usask.ca>
2025-03-22 09:28:09 -0600

    Moved deepseek_query out into its own file

--- fine_tune/fine_tune_deepseek.py
+++ fine_tune/fine_tune_deepseek.py
@@ -13,37 +13,9 @@ os.environ["HF_HOME"] = str(model_dir.absolute())
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from fine_tune.deepseek_query import DeepseekQuery
 
 TRAINING_DATASSET_DIR = pathlib.Path("downloaded_data/ctssb")
-
-# deepseek message format:
-# messages = [
-#     {"role": "system", "content": "You are a code fixer."},
-#     {"role": "user", "content": buggy_code},
-#     {"role": "assistant", "content": fixed_code},
-# ]
-
-
-class DeepseekQuery:
-    system_message: str = "You are a bot that detects and fixes single statement bugs in python modules."
-
-    def __init__(self, before_file: str, after_file: str):
-        self._before_file: str = before_file
-        self._after_file: str = after_file
-
-        self._query: list[dict[str, str]] = [
-            {"role": "system", "content": self.system_message},
-            {"role": "user", "content": self._before_file},
-            {"role": "assistant", "content": self._after_file},
-        ]
-
-    @property
-    def query(self):
-        return self._query
-
-    @property
-    def inference_query(self):
-        return [{"role": "system", "content": self.system_message}, {"role": "user", "content": self._before_file}]
 
 
 def prepare_queries() -> list[DeepseekQuery]:
